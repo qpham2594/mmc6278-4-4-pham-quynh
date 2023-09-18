aa078a6a5738aa9a5ce3d8ec71f72362dcac84f0
Quynh <qpham2594@gmail.com>
2023-09-18 15:38:39 -0400

    intialization and progress

---
+++ queries/05-join-artist-album.sql
@@ -0,0 +1,11 @@
+-- Join artists on albums, pulling artists.name as artist_name
+-- albums.name as album_name and albums.release_year
+-- sort by artist name and album name ascending
+
+
+SELECT artists.name AS artist_name,
+    albums.name AS album_name, 
+    albums.release_year
+FROM artists
+LEFT JOIN albums ON artists.name = albums.artist_id
+ORDER BY artist_name ASC, album_name ASC;
---
+++ queries/07-update.sql
@@ -0,0 +1,7 @@
+-- Metallica's Black Album is actually a self-titled album
+-- Change the name of The Black Album to "Metallica"
+
+
+UPDATE albums
+SET name = 'Metallica'
+WHERE name = 'The Black Album';
